9cef6ade012b0373ef11fa849a84537da542311b
WinterHana <haha920@naver.com>
2023-05-09 00:57:53 +0900

    config 변경 테스트

--- venvs/myproject.cmd
+++ venvs/myproject.cmd
@@ -1,5 +1,6 @@
 @echo off
-cd C:/Project/Flask
-set FLASK_APP=pybo
+cd C:/Project/Flask/myproject
+SET FLASK_APP=pybo
 SET FLASK_DEBUG=true
+SET APP_CONFIG_FILE=C:\Project\Flask\myproject\config\development.py
 c:/Project/Flask/venvs/FPS/Scripts/activate
